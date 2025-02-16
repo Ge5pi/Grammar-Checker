44516e7e2ca92a48855f4c6311034a25ce9b303c
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-02-16 14:32:19 +0500

    pocede text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import nest_asyncio
-
+from g4f.client import Client
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -18,3 +18,16 @@ nest_asyncio.apply()
 async def start(message: Message):
     text = f"Hi! I am a bot to check your grammar and punctuation. Send me your message and I will edit it."
     await message.answer(text, parse_mode="Markdown")
+
+
+@dis.message()
+async def handle_message(message: Message):
+    client = Client()
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": f"Hi! Check my text on grammar and punctuation mistakes: \"{message.text}\""},
+            {"role": "system", "content": "Answer with this template and only like this: \"Corrected words:\n\nRules you should read:\", If you did not find mistakes, say it."}
+        ]
+    )
+    await message.answer(response.choices[0].message.content)
